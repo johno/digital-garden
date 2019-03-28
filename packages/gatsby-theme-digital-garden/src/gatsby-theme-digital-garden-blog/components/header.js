6a68db424691d14b43ab55a79166d8672b89d8a2
John Otander <johnotander@gmail.com>
2019-03-28 10:39:11 -0600

    Fix typo

--- packages/gatsby-theme-digital-garden/src/gatsby-theme-digital-garden-blog/components/header.js
+++ packages/gatsby-theme-digital-garden/src/gatsby-theme-digital-garden-blog/components/header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 import { Header } from 'gatsby-theme-system'
 
-import { Box } from '../../compponents/ui'
+import { Box } from '../../components/ui'
 import useSiteMetadata from '../../use-site-metadata'
 
 export default () => {
